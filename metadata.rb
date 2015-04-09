94f0939dd83b3c2aee1009fbbc580bdd352ecec8
simplyadrian <simplyadrian@gmail.com>
2015-04-09 09:55:07 -0500

    ISE-271 update to metadata.json to correct version and maintainer information.

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license 'Apache 2.0'
 description 'LWRPs for managing AWS resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.7.0'
-recipe 'aws', 'Installs the right_aws gem during compile time'
+recipe 'aws', 'Installs the aws_sdk gem during compile time'
 source_url       "https://github.com/opscode-cookbooks/aws"
 issues_url       "https://github.com/opscode-cookbooks/aws/issues"
